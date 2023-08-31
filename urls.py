df505f9ede2a8b301b0474307c8d48966165c2d3
Andrey Murgin <murgin.andy@gmail.com>
2023-08-31 13:42:56 +0300

    do not stage pycharm files

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 class UrlMaker:
+    @staticmethod
     def post_message_url(channel_id):
         return f"https://discord.com/api/v9/channels/{channel_id}/messages"
 
+    @staticmethod
     def post_command_url():
         return "https://discord.com/api/v9/interactions"
