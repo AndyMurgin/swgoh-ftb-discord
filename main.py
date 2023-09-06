d49c1ec1b9d5c2f9ab3f8ccf5487c73153362ff0
Andrey Murgin <murgin.andy@gmail.com>
2023-09-07 00:07:20 +0300

    tag channel members if they can be recognized by the nickname

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 from account_sender import OwnerAccountSender
 from configs import PropertiesHolder
+from seals_finder import Hunter
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 ownerSender = OwnerAccountSender(PropertiesHolder.get_owner_token())
@@ -24,7 +25,6 @@ async def on_message(message):
 async def on_message_edit(before, after: Message):
     if (
         not after.author.id == 752366060312723546
-        or not after.author.name == "C3PO"
         or not after.interaction
         or not after.interaction.name == "tb gp low"
         or not len(after.embeds) == 1
@@ -33,8 +33,31 @@ async def on_message_edit(before, after: Message):
 
     seals = [x.name for x in after.embeds[0].fields]
     ctx = await client.get_context(after)
-    await ctx.send("WTF??? Быстро бить ТБ!")
-    await ctx.send("\n".join(seals))
+
+    if len(seals) > 0:
+        grouped_members = Hunter.find_seal_members(seals, ctx)
+        await ctx.send("WTF??? Быстро бить ТБ!")
+        if len(grouped_members.found_members) > 0:
+            await ctx.send(
+                "\n".join(
+                    [
+                        member.mention
+                        for nickname, member in grouped_members.found_members.items()
+                    ]
+                )
+            )
+
+        if len(grouped_members.unrecognized) > 0:
+            await ctx.send(
+                "Не смог найти некоторых тюленей в этом канале. Добавьте их вручную (пока не реализовано) "
+                "или идите за ними в Telegram:"
+            )
+            await ctx.send(
+                "\n".join([nickname for nickname in grouped_members.unrecognized])
+            )
+
+    else:
+        await ctx.send("Тюлени не обнаружены. Кажется, я попал не в Tython_LEPV.")
 
     await client.process_commands(after)
 
---
+++ seals_finder.py
@@ -0,0 +1,30 @@
+from discord.ext.commands import Context
+
+
+class SealMembers:
+    found_members = {}
+    unrecognized = []
+
+
+class Hunter:
+    @staticmethod
+    def find_seal_members(seals: list, ctx: Context) -> SealMembers:
+        grouped_members = SealMembers()
+        channel_members = ctx.channel.members
+        grouped_members.found_members = {
+            nickname: member
+            for nickname in seals
+            for member in channel_members
+            if (
+                member.display_name.find(nickname)
+                if member.display_name is not None
+                else -1
+            )
+            >= 0
+        }
+        grouped_members.unrecognized = [
+            nickname
+            for nickname in seals
+            if nickname not in grouped_members.found_members
+        ]
+        return grouped_members
